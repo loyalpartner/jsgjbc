c5dabb3cd6d32a22135694eceab80bec41094a87
lee <loyalpartner@163.com>
2021-07-12 17:14:26 +0800

    refactor: 9.1.1 throw exception

--- __tests__/ch09/9-1_test.js
+++ __tests__/ch09/9-1_test.js
@@ -17,16 +17,8 @@ test("9.1.3 创建空代理", ()=>{
   expect(target.hasOwnProperty('id')).toBeTruthy();
   expect(proxy.hasOwnProperty('id')).toBeTruthy();
 
-  try{
-    target instanceof Proxy;
-  }catch(e){
-    expect(e).not.toBeNull();
-  }
-  try{
-    proxy instanceof Proxy;
-  }catch(e){
-    expect(e).not.toBeNull();
-  }
+  expect(()=> target instanceof Proxy).toThrow(TypeError)
+  expect(()=> proxy instanceof Proxy).toThrow(TypeError)
 
   expect(target === proxy).not.toBeTruthy();
 })
@@ -58,11 +50,61 @@ test("9.1.3 捕获器参数和反射 API", ()=>{
       return trapTarget[property];
     }
   };
-  
   const proxy = new Proxy(target, handler);
 
   expect(proxy.foo).toEqual('bar')
   expect(target.foo).toEqual('bar')
 
+  const target2 = { foo: 'bar' }
+  const handler2 = {
+    // get(){
+    //   return Reflect.get(...arguments)
+    // }
+    get: Reflect.get // 和上面的注释等价
+  };
+  const proxy2 = new Proxy(target2, handler2);
+
+  expect(proxy2.foo).toEqual('bar')
+  expect(target2.foo).toEqual('bar')
+
+  const target3 = { foo: 'bar', baz: "qux" }
+  const handler3 = {
+    get(trapTarget, property, receiver){
+      let decoration = '';
+      if (property === 'foo'){
+        decoration = '!!!'
+      }
+      return Reflect.get(...arguments) + decoration;
+    }
+  };
+  const proxy3 = new Proxy(target3, handler3);
+
+  expect(proxy3.foo).toEqual('bar!!!')
+  expect(target3.foo).toEqual('bar')
+
+  expect(proxy3.baz).toEqual('qux')
+  expect(target3.baz).toEqual('qux')
 });
 
+test("9.1.4 捕获器不变式", ()=>{
+  const target = {}
+  Object.defineProperty(target, 'foo', {
+    configurable: false,
+    writable: false,
+    value: 'bar'
+  })
+
+  const handler = {
+    get(){
+      return 'qux'
+    }
+  }
+
+  const proxy = new Proxy(target, handler);
+
+  expect.assertions(1)
+  try { proxy.foo } catch(e){
+    expect(e).not.toBeNull()
+  }
+
+})
